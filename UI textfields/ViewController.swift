dd02d35ceaf8af488b9a761e003c91c81f8f85aa
TahaMagdy <tahamagdy314@gmail.com>
2018-07-22 17:01:17 +0200

    Initial Commit

---
+++ UI textfields/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UI textfields
+//
+//  Created by Taha Magdy on 7/22/18.
+//  Copyright © 2018 Taha Magdy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
